a58184b6afedbf7bd1ba7c516d38a69930f39ae2
darenc9 <devoncen@gmail.com>
2024-10-14 15:44:46 -0400

    Initialized spring boot app

---
+++ Springboot-Backend/financial-dashboard/src/main/java/com/teamninjas/financial_dashboard/FinancialDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.teamninjas.financial_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialDashboardApplication.class, args);
+	}
+
+}
